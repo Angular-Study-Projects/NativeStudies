dd0d1058dec458a19403f4f98b2b90da23f01e57
lpgoulart <leonardo@roffor.com.br>
2019-11-26 09:55:51 -0300

    create some content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 import { SegmentedBar, SegmentedBarItem } from "tns-core-modules/ui/segmented-bar";
-import { ItemEventData } from "tns-core-modules/ui/list-view/list-view";
+import { ItemEventData, ListView } from "tns-core-modules/ui/list-view/list-view";
 import { SearchBar } from "tns-core-modules/ui/search-bar";
+
+import { EventData, fromObject } from "tns-core-modules/data/observable";
+import { Page } from "tns-core-modules/ui/page";
 
 
 @Component({
@@ -11,41 +14,42 @@ import { SearchBar } from "tns-core-modules/ui/search-bar";
     templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
-    [x: string]: any;
-
+    routerExtensions: any;
+ 
     constructor() {
         // Use the component constructor to inject providers.
     }
 
     countries: { name: string, imageSrc: string }[] = [
-        { name: "Australia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/au.png" },
-        { name: "Belgium", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/be.png" },
-        { name: "Bulgaria", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/bg.png" },
-        { name: "Canada", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ca.png" },
-        { name: "Switzerland", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ch.png" },
-        { name: "China", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/cn.png" },
-        { name: "Czech Republic", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/cz.png" },
-        { name: "Germany", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/de.png" },
-        { name: "Spain", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/es.png" },
-        { name: "Ethiopia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/et.png" },
-        { name: "Croatia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/hr.png" },
-        { name: "Hungary", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/hu.png" },
-        { name: "Italy", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/it.png" },
-        { name: "Jamaica", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/jm.png" },
-        { name: "Romania", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ro.png" },
-        { name: "Russia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ru.png" },
-        { name: "United States", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/us.png" },
+        { name: "Australia", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Belgium", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Bulgaria", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Canada", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Switzerland", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "China", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Czech Republic", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Germany", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Spain", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Ethiopia", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Croatia", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Hungary", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Italy", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Jamaica", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Romania", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "Russia", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
+        { name: "United States", imageSrc: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/folder-blue-512.png" },
     ];
 
-    onItemTap(args: ItemEventData): void {
-        console.log('Item with index: ' + args.index + ' tapped');
-    }
     private getSegmentedBarItems = () => {
         let segmentedBarItem1 = new SegmentedBarItem();
         segmentedBarItem1.title = "Item 1";
         let segmentedBarItem2 = new SegmentedBarItem();
         segmentedBarItem2.title = "Item 2";
-        return [segmentedBarItem1, segmentedBarItem2];
+        let segmentedBarItem3 = new SegmentedBarItem();
+        segmentedBarItem3.title = "Item 3";
+        // segmentedBarItem3.row
+
+        return [segmentedBarItem1, segmentedBarItem2, segmentedBarItem3];
     }
     segmentedBarItems: Array<SegmentedBarItem> = this.getSegmentedBarItems();
     selectedBarIndex: number = 0;
@@ -87,4 +91,5 @@ export class HomeComponent implements OnInit {
         const searchBar = args.object as SearchBar;
         console.log(`Clear event raised`);
     }
+
 }
