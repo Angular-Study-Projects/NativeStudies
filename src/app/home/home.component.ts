f34e3b4fa1ae9270534b82c61078a56d7ed00d6e
lpgoulart <leonardo@roffor.com.br>
2019-11-26 08:35:54 -0300

    first upload

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from "@angular/core";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import * as app from "tns-core-modules/application";
+import { SegmentedBar, SegmentedBarItem } from "tns-core-modules/ui/segmented-bar";
+import { ItemEventData } from "tns-core-modules/ui/list-view/list-view";
+import { SearchBar } from "tns-core-modules/ui/search-bar";
+
+
+@Component({
+    selector: "Home",
+    templateUrl: "./home.component.html"
+})
+export class HomeComponent implements OnInit {
+    [x: string]: any;
+
+    constructor() {
+        // Use the component constructor to inject providers.
+    }
+
+    countries: { name: string, imageSrc: string }[] = [
+        { name: "Australia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/au.png" },
+        { name: "Belgium", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/be.png" },
+        { name: "Bulgaria", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/bg.png" },
+        { name: "Canada", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ca.png" },
+        { name: "Switzerland", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ch.png" },
+        { name: "China", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/cn.png" },
+        { name: "Czech Republic", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/cz.png" },
+        { name: "Germany", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/de.png" },
+        { name: "Spain", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/es.png" },
+        { name: "Ethiopia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/et.png" },
+        { name: "Croatia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/hr.png" },
+        { name: "Hungary", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/hu.png" },
+        { name: "Italy", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/it.png" },
+        { name: "Jamaica", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/jm.png" },
+        { name: "Romania", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ro.png" },
+        { name: "Russia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ru.png" },
+        { name: "United States", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/us.png" },
+    ];
+
+    onItemTap(args: ItemEventData): void {
+        console.log('Item with index: ' + args.index + ' tapped');
+    }
+    private getSegmentedBarItems = () => {
+        let segmentedBarItem1 = new SegmentedBarItem();
+        segmentedBarItem1.title = "Item 1";
+        let segmentedBarItem2 = new SegmentedBarItem();
+        segmentedBarItem2.title = "Item 2";
+        return [segmentedBarItem1, segmentedBarItem2];
+    }
+    segmentedBarItems: Array<SegmentedBarItem> = this.getSegmentedBarItems();
+    selectedBarIndex: number = 0;
+
+
+    ngOnInit(): void {
+        // Init your component properties here.
+    }
+
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>app.getRootView();
+        sideDrawer.showDrawer();
+    }
+
+    onNavItemTap(navItemRoute: string): void {
+        this.routerExtensions.navigate([navItemRoute], {
+            transition: {
+                name: "fade"
+            }
+        });
+
+        const sideDrawer = <RadSideDrawer>app.getRootView();
+        sideDrawer.closeDrawer();
+    }
+
+    searchPhrase: string;
+
+    onSubmit(args) {
+        const searchBar = args.object as SearchBar;
+        console.log(`Searching for ${searchBar.text}`);
+    }
+
+    onTextChanged(args) {
+        const searchBar = args.object as SearchBar;
+        console.log(`Input changed! New value: ${searchBar.text}`);
+    }
+
+    onClear(args) {
+        const searchBar = args.object as SearchBar;
+        console.log(`Clear event raised`);
+    }
+}
